6328b6438c74864c79b8192c4e908dcb7c3bfe46
Vinicius Dias <carlosv775@gmail.com>
2022-05-21 16:55:20 -0300

    4.1

---
+++ src/Entity/Phone.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Alura\Doctrine\Entity;
+
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+
+#[Entity]
+class Phone
+{
+    #[Id]
+    #[GeneratedValue]
+    #[Column]
+    public int $id;
+
+    public function __construct(
+        #[Column]
+        public readonly string $number
+    ) {
+    }
+}
