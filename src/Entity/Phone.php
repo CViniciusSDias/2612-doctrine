25dd6bdb99be0293c5938e1204f7725498843fb2
Vinicius Dias <carlosv775@gmail.com>
2022-05-21 17:20:53 -0300

    4.2

--- src/Entity/Phone.php
+++ src/Entity/Phone.php
@@ -6,18 +6,25 @@ use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\ManyToOne;
 
 #[Entity]
 class Phone
 {
-    #[Id]
-    #[GeneratedValue]
-    #[Column]
+    #[Id, GeneratedValue, Column]
     public int $id;
+
+    #[ManyToOne(targetEntity: Student::class, inversedBy: "phones")]
+    public readonly Student $student;
 
     public function __construct(
         #[Column]
         public readonly string $number
     ) {
+    }
+
+    public function setStudent(Student $student): void
+    {
+        $this->student = $student;
     }
 }
--- src/Entity/Student.php
+++ src/Entity/Student.php
@@ -2,22 +2,38 @@
 
 namespace Alura\Doctrine\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\OneToMany;
 
 #[Entity]
 class Student
 {
-    #[Id]
-    #[GeneratedValue]
-    #[Column]
+    #[Id, GeneratedValue, Column]
     public int $id;
+
+    #[OneToMany(targetEntity: Phone::class, mappedBy: "student")]
+    private readonly Collection $phones;
 
     public function __construct(
         #[Column]
         public readonly string $name
     ) {
+        $this->phones = new ArrayCollection();
+    }
+
+    public function addPhone(Phone $phone)
+    {
+        $this->phones->add($phone);
+        $phone->setStudent($this);
+    }
+
+    public function phones(): iterable
+    {
+        return $this->phones;
     }
 }
