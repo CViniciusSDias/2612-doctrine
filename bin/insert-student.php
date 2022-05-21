20a202c4a1636835c5a54e0c1b26aac58934828a
Vinicius Dias <carlosv775@gmail.com>
2022-05-21 14:43:00 -0300

    Video 2.3

---
+++ bin/insert-student.php
@@ -0,0 +1,13 @@
+<?php
+
+use Alura\Doctrine\Entity\Student;
+use Alura\Doctrine\Helper\EntityManagerCreator;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$entityManager = EntityManagerCreator::createEntityManager();
+
+$student = new Student('Vinicius Dias');
+
+$entityManager->persist($student);
+$entityManager->flush();
