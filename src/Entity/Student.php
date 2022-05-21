cd7ef5c1ea29f953d9ec49c7aad5a7c441d5cf22
Vinicius Dias <carlosv775@gmail.com>
2022-05-21 14:14:57 -0300

    Video 2.1

---
+++ src/Entity/Student.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Alura\Doctrine\Entity;
+
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+
+#[Entity]
+class Student
+{
+    #[Id]
+    #[GeneratedValue]
+    #[Column]
+    public readonly int $id;
+
+    public function __construct(
+        #[Column]
+        public readonly string $name
+    ) {
+    }
+}
